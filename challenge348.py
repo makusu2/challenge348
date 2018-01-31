fe0916f806d0a969d1746c5490f1fe73c4ad8f3a
Pitts <pittss@wit.edu>
2018-01-31 15:13:28 -0500

    Super fast

--- challenge348.py
+++ challenge348.py
@@ -17,61 +17,72 @@ def timeit(method):
             print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
         return result
     return timed
-class NoSolutionsException(BaseException):
-    pass
-
-
-@timeit
-def getSolution(maxNum):
-    def pairMeetsReqs(n1,n2):
-        def isWholeNum(n):
-            return n == int(n)
-        return isWholeNum(math.sqrt(n1+n2))
-        
-    def allowableNextRepetition(permList,nums):
-        return (num for num in nums if num not in permList)
-    def getSquareList(maxNum):
-        maxSquare = int(math.sqrt(maxNum*2 - 1))
-        squares = tuple(n**2 for n in range(1,maxSquare+1))
-        return squares
-        
-    def canBeNext(permList,nextVal):
-        return pairMeetsReqs(permList[-1],nextVal)
-    def allowableNextComplete(permList,nums):
-        return (num for num in allowableNextRepetition(permList,nums) if canBeNext(permList,num))
-    def asAppendedVal(permList,num):
-        return list(permList)+[num]
-    def removeVal(collection):
-        return collection.get()[1]
-    def addPerm(collection,newPerm,nums):
-        newPerm = list(newPerm)
-        allowableNexts = tuple(allowableNextComplete(newPerm,nums))
-        #print(len(allowableNexts))
+    
+    
+    
+class Agent348:
+    def __init__(self,maxNum):
+        self.maxNum = maxNum
+        self.nums = tuple(range(1,self.maxNum+1))
+        self.col = PriorityQueue()
+        self.possibleSquares = self.getSquares()
+        self.allowableNextDict = self.getAllowableNextDict()
+    
+    def getAllowableNextDict(self):
+        d = {i:tuple(j for j in range(1,self.maxNum+1) if j!=i and self.pairMeetsReqs(j,i)) for i in range(1,self.maxNum+1)}
+        return d
+    def getSquares(self):
+        maxSum = 2*max(self.nums) - 1
+        maxSquared = int(math.sqrt(maxSum))
+        allowableSquares = tuple(n**2 for n in range(1,maxSquared+1))
+        return allowableSquares
+    def pairMeetsReqs(self,n1,n2):
+        return n1+n2 in self.possibleSquares
+    def allowableNextRepetition(self,perm):
+        return (num for num in self.nums if num not in perm)
+    def canBeNext(self,perm,nextVal):
+        return self.pairMeetsReqs(perm[-1],nextVal)
+    def allowableNextComplete(self,perm):
+        return (num for num in self.allowableNextRepetition(perm) if self.canBeNext(perm,num))
+    def asAppendedVal(self,perm,num):
+        return list(perm)+[num]
+    def removeVal(self):
+        return self.col.get()[1]
+    def addPerm(self,perm):
+        perm = tuple(perm)
+        allowableNexts = self.allowableNextFast(perm)
         priority = len(allowableNexts)
         if priority != 0:
-            #print(priority)
-            collection.put((len(allowableNexts),(newPerm,allowableNexts)))
-        
-    #currentPerms contains a tuple of: (actual perm, list possibleNexts)
-    squares = getSquareList(maxNum)
-    nums = tuple(range(1,maxNum+1))
-    currentPerms = PriorityQueue()
-    for perm in itertools.permutations(nums,2):
-        if pairMeetsReqs(*perm):
-            addPerm(currentPerms,perm,nums)
-    
-    while not currentPerms.empty():
-        perm,allowableNexts = removeVal(currentPerms)
-        for nextVal in allowableNexts:
-            permChild = asAppendedVal(perm,nextVal)
-            if len(permChild) == len(nums):
-                return permChild
-            else:
-                addPerm(currentPerms,permChild,nums)
-    return None
+            self.col.put((priority + (1/len(perm)),(perm,allowableNexts)))
+            
+    def allowableNextFast(self,perm):
+        return tuple(n for n in self.allowableNextDict[perm[-1]] if n not in perm)
+    @timeit
+    def getSolution(self):
+        nodesExpanded = 0
+        for perm in itertools.permutations(self.nums,2):
+            if self.pairMeetsReqs(*perm):
+                self.addPerm(perm)
+        #start time consumption
+        while not self.col.empty():
+            perm,allowableNexts = self.removeVal()
+            nodesExpanded += 1
+            #if len(perm) == 2:
+                #print('got to perm ',perm)
+            for nextVal in allowableNexts:
+                permChild = self.asAppendedVal(perm,nextVal)
+                if len(permChild) == len(self.nums):
+                    print('nodes expanded: ',nodesExpanded)
+                    return permChild
+                else:
+                    self.addPerm(permChild)
+        print('nodes expanded: ',nodesExpanded)
+        return None
 
 def testMax(maxNum):
-    sol = getSolution(maxNum)
+    print('With node priority:')
+    agent = Agent348(maxNum)
+    sol = agent.getSolution()
     if sol is None:
         print(maxNum,' has no solution')
     else:
@@ -79,7 +90,8 @@ def testMax(maxNum):
 try:
     maxNum = int(sys.argv[1])
 except IndexError:
-    for i in range(3,50):
+    for i in range(20,100):
+        print('\n\nTesting ',i)
         testMax(i)
 else:
     testMax(maxNum)
