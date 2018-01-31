a948128b49afcb8eb91fc8e9312b7867b425a391
Pitts <pittss@wit.edu>
2018-01-31 12:12:34 -0500

    More generic but I want to just do dfs and optimize

--- challenge348.py
+++ challenge348.py
@@ -2,6 +2,7 @@ import sys
 import math
 import itertools
 import time
+from collections import deque
 
 def pairMeetsReqs(n1,n2):
     def isWholeNum(n):
@@ -27,53 +28,12 @@ def timeit(method):
             print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
         return result
     return timed
-@timeit
-def getSolution(maxNum,genmaker):
-    nums = range(1,maxNum+1)
-    try:
-        for testVals in genMaker(nums):
-            if meetsReqs(testVals):
-                return testVals
-    except NoSolutionsException:
-        return None
 class NoSolutionsException(BaseException):
     pass
-class Stack:
-     def __init__(self):
-         self.items = []
 
-     def isEmpty(self):
-         return self.items == []
 
-     def push(self, item):
-         self.items.append(item)
-
-     def pop(self):
-         return self.items.pop()
-
-     def peek(self):
-         return self.items[len(self.items)-1]
-
-     def size(self):
-         return len(self.items)
-         
-class Queue:
-    def __init__(self):
-        self.items = []
-
-    def isEmpty(self):
-        return self.items == []
-
-    def enqueue(self, item):
-        self.items.insert(0,item)
-
-    def dequeue(self):
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
-
-def genMaker(nums):
+@timeit
+def getSolution(maxNum,searchType='bfs'):
     def allowableNextRepetition(permList,nums):
         return (num for num in nums if num not in permList)
     def canBeNext(permList,nextVal):
@@ -82,19 +42,26 @@ def genMaker(nums):
         return (num for num in allowableNextRepetition(permList,nums) if canBeNext(permList,num))
     def asAppendedVal(permList,num):
         return permList+[num]
-    container = Queue()
-    nums = list(nums)
-    startPerms = (list(perm) for perm in itertools.permutations(nums,2) if meetsReqs(perm))
-    currentPerms = startPerms
-    curLen = 2
-    if not currentPerms:
-        raise NoSolutionsException
-    while(curLen < len(nums)):
-        currentPerms = (asAppendedVal(perm,nextVal) for perm in currentPerms for nextVal in allowableNextComplete(perm,nums))
-        curLen+=1
-    return currentPerms
+    def removeVal(collection,bfs):
+        return collection.popleft() if bfs else collection.pop()
+        
+    assert(searchType in ['bfs','dfs'])
+    bfs = searchType == 'bfs'
+    nums = tuple(range(1,maxNum+1))
+    currentPerms = deque([list(perm) for perm in itertools.permutations(nums,2) if meetsReqs(perm)])
+    
+    while(currentPerms):
+        perm = removeVal(currentPerms,bfs)
+        for nextVal in allowableNextComplete(perm,nums):
+            addVal = asAppendedVal(perm,nextVal)
+            if len(addVal) == len(nums):
+                return addVal
+            else:
+                currentPerms.append(addVal)
+    return None
+
 def testMax(maxNum):
-    sol = getSolution(maxNum,genMaker)
+    sol = getSolution(maxNum,searchType='dfs')
     if sol is None:
         print(maxNum,' has no solution')
     else:
